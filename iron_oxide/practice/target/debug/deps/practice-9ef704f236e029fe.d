ad2a422d2f396ccfe2d5b55c98e86de0adb1de9a
BenjaminTollison <benjamin.tollison@outlook.com>
2023-11-24 22:25:37 -0600

    finally got rust to run

---
+++ iron_oxide/practice/target/debug/deps/practice-9ef704f236e029fe.d
@@ -0,0 +1,5 @@
+C:\Users\benja\OneDrive\Documents\GitHub\School23\iron_oxide\practice\target\debug\deps\libpractice-9ef704f236e029fe.rmeta: src\main.rs
+
+C:\Users\benja\OneDrive\Documents\GitHub\School23\iron_oxide\practice\target\debug\deps\practice-9ef704f236e029fe.d: src\main.rs
+
+src\main.rs:
---
+++ iron_oxide/practice/target/debug/deps/practice-d51d0c4358091a2c.d
@@ -0,0 +1,5 @@
+C:\Users\benja\OneDrive\Documents\GitHub\School23\iron_oxide\practice\target\debug\deps\libpractice-d51d0c4358091a2c.rmeta: src\main.rs
+
+C:\Users\benja\OneDrive\Documents\GitHub\School23\iron_oxide\practice\target\debug\deps\practice-d51d0c4358091a2c.d: src\main.rs
+
+src\main.rs:
